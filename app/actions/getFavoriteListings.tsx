d1379ec3ba3057b030674b254e9dc3d8c02ec490
Oraka Emmanuel <oraka.emmanuel@gmail.com>
2023-07-04 10:40:10 -0700

    added

--- app/actions/getFavoriteListings.tsx
+++ app/actions/getFavoriteListings.tsx
@@ -1,30 +1,30 @@
-// import prisma from "@/app/libs/prismadb";
+import prisma from "@/app/libs/prismadb";
 
-// import getCurrentUser from "./getCurrentUser";
+import getCurrentUser from "./getCurrentUser";
 
-// export default async function getFavoriteListings() {
-//   try {
-//     const currentUser = await getCurrentUser();
+export default async function getFavoriteListings() {
+  try {
+    const currentUser = await getCurrentUser();
 
-//     if (!currentUser) {
-//       return [];
-//     }
+    if (!currentUser) {
+      return [];
+    }
 
-//     const favorites = await prisma.listing.findMany({
-//       where: {
-//         id: {
-//           in: [...(currentUser.favoriteIds || [])],
-//         },
-//       },
-//     });
+    const favorites = await prisma.listing.findMany({
+      where: {
+        id: {
+          in: [...(currentUser.favoriteIds || [])],
+        },
+      },
+    });
 
-//     const safeFavorites = favorites.map((favorite) => ({
-//       ...favorite,
-//       createdAt: favorite.createdAt.toString(),
-//     }));
+    const safeFavorites = favorites.map((favorite) => ({
+      ...favorite,
+      createdAt: favorite.createdAt.toString(),
+    }));
 
-//     return safeFavorites;
-//   } catch (error: any) {
-//     throw new Error(error);
-//   }
-// }
+    return safeFavorites;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,37 +1,37 @@
-// import EmptyState from "../components/EmptyState";
-// import ClientOnly from "../components/ClientOnly";
-// import getCurrentUser from "../actions/getCurrentUser";
-// import getReservations from "../actions/getReservation";
-// import PropertiesClient from "./PropertiesClient";
-// import getListings from "../actions/getListings";
+import EmptyState from "../components/EmptyState";
+import ClientOnly from "../components/ClientOnly";
+import getCurrentUser from "../actions/getCurrentUser";
+import getReservations from "../actions/getReservation";
+import PropertiesClient from "./PropertiesClient";
+import getListings from "../actions/getListings";
 
-// const PropertiesPage = async () => {
-//   const currentUser = await getCurrentUser();
-//   if (!currentUser) {
-//     return (
-//       <ClientOnly>
-//         <EmptyState title="Unauthorised" subtitle="please login " />
-//       </ClientOnly>
-//     );
-//   }
+const PropertiesPage = async () => {
+  const currentUser = await getCurrentUser();
+  if (!currentUser) {
+    return (
+      <ClientOnly>
+        <EmptyState title="Unauthorised" subtitle="please login " />
+      </ClientOnly>
+    );
+  }
 
-//   const listings = await getListings({ userId: currentUser.id });
-//   if (listings.length === 0) {
-//     return (
-//       <ClientOnly>
-//         <EmptyState
-//           title="No properties found"
-//           subtitle="Looks like you have no properties "
-//         />
-//       </ClientOnly>
-//     );
-//   } else {
-//     return (
-//       <ClientOnly>
-//         <PropertiesClient listings={listings} currentUser={currentUser} />
-//       </ClientOnly>
-//     );
-//   }
-// };
+  const listings = await getListings({ userId: currentUser.id });
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title="No properties found"
+          subtitle="Looks like you have no properties "
+        />
+      </ClientOnly>
+    );
+  } else {
+    return (
+      <ClientOnly>
+        <PropertiesClient listings={listings} currentUser={currentUser} />
+      </ClientOnly>
+    );
+  }
+};
 
-// export default PropertiesPage;
+export default PropertiesPage;
