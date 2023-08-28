64e50d4f748c95192ffd9fa772bcf9a9352a3194
Oraka Emmanuel <oraka.emmanuel@gmail.com>
2023-08-28 22:46:36 +0100

    added

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { useState, useCallback, useEffect } from "react";
-import { AiOutlineMenu } from "react-icons/ai";
+import { useState, useCallback, useEffect, useRef } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
 import MenuItem from "./MenuItem";
 import useRegisterModal from "../hooks/useRegisterModal";
@@ -9,7 +9,7 @@ import { signOut } from "next-auth/react";
 import { SafeUser } from "@/app/types";
 import useRentModal from "../hooks/useRentModal";
 import { useRouter } from "next/navigation";
-import { User } from "@prisma/client";
+import { Dialog } from "@headlessui/react";
 
 interface UserMenuProps {
   currentUser?: SafeUser | null;
@@ -22,10 +22,11 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
 
   const rentModal = useRentModal();
   const [isOpen, setIsOpen] = useState(false);
+  const [openMenu, setOpenMenu] = useState(false);
 
-  const toggleOpen = () => {
-    setIsOpen((value) => !value);
-  };
+  // const toggleOpen = () => {
+  //   setMenu((value) => !value);
+  // };
 
   const onRent = useCallback(() => {
     if (!currentUser) {
@@ -35,72 +36,80 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
     rentModal.onOpen();
   }, [loginModal, currentUser, rentModal]);
 
-  // useEffect(() => {
-  //   document.addEventListener("mousedown", () => {
-  //     setIsOpen(false);
-  //   });
-  // }, []);
+  const onOpenMenu = () => setOpenMenu(true);
+  const onCloseMenu = () => setOpenMenu(false);
 
   return (
-    <div className="relative">
-      <div className="flex flex-row items-center gap-3">
-        <div
-          onClick={onRent}
-          className="
-            hidden
-            md:block
-            text-sm 
-            font-semibold 
-            py-3 
-            px-4 
-            rounded-full 
-            hover:bg-neutral-100 
-            transition 
-            cursor-pointer
-          "
-        >
-          Rentals
-        </div>
-        <div
-          onClick={toggleOpen}
-          className="
-          p-4
-          md:py-1
-          md:px-2
-          border-[1px] 
-          border-neutral-200 
-          flex 
-          flex-row 
-          items-center 
-          gap-3 
-          rounded-full 
-          cursor-pointer 
-          hover:shadow-md 
+    <div className="flex flex-row items-center gap-3">
+      <div
+        onClick={onRent}
+        className="
+          hidden
+          md:block
+          text-sm
+          font-semibold
+          py-3
+          px-4
+          rounded-full
+          hover:bg-neutral-100
           transition
-          "
-        >
-          <AiOutlineMenu />
-          <div className="hidden md:block">
-            <Avatar src={currentUser?.image} />
-          </div>
+          cursor-pointer
+        "
+      >
+        Rentals
+      </div>{" "}
+      <div
+        onClick={onOpenMenu}
+        className="
+        p-4
+        md:py-1
+        md:px-2
+        border-[1px]
+        border-neutral-200
+        flex
+        flex-row
+        items-center
+        gap-3
+        rounded-full
+        cursor-pointer
+        hover:shadow-md
+        transition
+        "
+      >
+        <AiOutlineMenu />
+
+        <div className="hidden md:block">
+          <Avatar src={currentUser?.image} />
         </div>
       </div>
-      {isOpen && (
+      <Dialog
+        open={openMenu}
+        as="div"
+        className="relative z-40 "
+        onClose={onCloseMenu}
+      >
+        {/* Background color and opacity */}
+        <div className="fixed inset-0 bg-black bg-opacity-25" />
+
+        {/* Dialog position */}
         <div
-          className="
-            absolute 
-            rounded-xl 
-            shadow-md
-            w-[40vw]
-            md:w-3/4 
-            bg-white 
-            overflow-hidden 
-            right-0 
-            top-12 
-            text-sm
-          "
+          className="fixed inset-
+         text-sm
+                
+              
+        z-40 flex top-0 right-0"
         >
-          <div className="flex flex-col cursor-pointer">
+          <Dialog.Panel
+            className="relative ml-auto
+           flex  w-full max-w-xs flex-col
+            overflow-y- bg-white py-4 pb-6 shadow-xl"
+          >
+            {/* Close button */}
+            <div className="flex items-center justify-end px-4">
+              <div onClick={onCloseMenu}>
+                <AiOutlineClose />
+              </div>
+            </div>
             {currentUser ? (
               <>
                 <MenuItem
@@ -129,9 +138,9 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                 <MenuItem label="Sign up" onClick={registerModal.onOpen} />
               </>
             )}
-          </div>
+          </Dialog.Panel>
         </div>
-      )}
+      </Dialog>
     </div>
   );
 };
